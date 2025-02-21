491a703a69f5d8cc5c9b47b9c7fa4fde2b988b52
TheOneWithChair <cherry.studies19@gmail.com>
2025-02-21 21:24:45 +0530

    Enhance Interview page layout and webcam functionality; add job details display and toggle for webcam

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -4,9 +4,10 @@ import React, { useEffect, useState } from 'react'
 import { db } from "@/utils/db"
 import { MockInterview } from "@/utils/schema"
 import { eq } from "drizzle-orm"
-import { WebcamIcon } from 'lucide-react'
+import { Lightbulb, WebcamIcon } from "lucide-react"
 import { Button } from "@/components/ui/Button"
-import ReactWebcam from "react-webcam"
+import Webcam from "react-webcam";
+import Link from "next/link"
 
 export default function Interview({ params }) {
   const interviewId = React.use(params).interviewId
@@ -32,27 +33,66 @@ export default function Interview({ params }) {
     }
   }
 
-  return (
+  return  (
     <div className="my-10">
       <h2 className="font-bold text-2xl text-center">Let's Get Started</h2>
-      <div>
-        {webCamEnabled ? (
-          <div className="flex items-center justify-center p-10">
-            <ReactWebcam
-              onUserMedia={() => setWebCamEnabled(true)}
-              onError={() => setWebCamEnabled(false)}
-              height={300}
-              width={300}
-              mirrored="true"
-            />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 ">
+        <div className="flex flex-col my-5 gap-5">
+          <div className="flex flex-col p-5 rounded-lg border gap-5">
+            <h2 className="text-lg">
+              <strong>Job Role/Job Position: </strong>
+              {interviewData?.jobPosition}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Job Description/Job Stack: </strong>
+              {interviewData?.jobDesc}
+            </h2>
+            <h2 className="text-lg">
+              <strong>Years of Experience: </strong>
+              {interviewData?.jobExperience}
+            </h2>
           </div>
-        ) : (
+          <div className="p-5 border rounded-lg border-yellow-300 bg-yellow-100">
+            <h2 className="flex gap-2 items-center text-yellow-700 mb-2">
+              <Lightbulb />
+              <strong>Information</strong>
+            </h2>
+            <h2 className="mt-3 text-yellow-500">
+              {process.env.NEXT_PUBLIC_INFORMATION}
+            </h2>
+          </div>
+        </div>
+        <div>
+          {webCamEnabled ? (
+            <div className=" flex items-center justify-center p-10">
+              <Webcam
+                onUserMedia={() => setWebCamEnabled(true)}
+                onUserMediaError={() => setWebCamEnabled(false)}
+                height={300}
+                width={300}
+                mirrored={true}
+              />
+            </div>
+          ) : (
+            <div>
+              <WebcamIcon className="h-72 w-full my-6 p-20 bg-secondary rounded-lg border" />
+            </div>
+          )}
           <div>
-            <WebcamIcon className="h-72 w-full my-6 p-20 bg-secondary rounded-lg border" />
-            <Button onClick={() => setWebCamEnabled(true)}>Enable Webcam</Button>
+            <Button
+              className={`${webCamEnabled ? "w-full" : "w-full"}`}
+              onClick={() => setWebCamEnabled((prev) => !prev)}
+            >
+              {webCamEnabled ? "Close WebCam" : "Enable WebCam"}
+            </Button>
           </div>
-        )}
+        </div>
+      </div>
+      <div className="flex justify-center my-4 md:my-0 md:justify-end md:items-end">
+        <Link href={`/dashboard/interview/${interviewId}/start`}>
+          <Button>Start Interview</Button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
