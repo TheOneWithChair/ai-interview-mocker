d06b6dc0df1a00e9c081f4ff3f421b7ef59aa19a
TheOneWithChair <cherry.studies19@gmail.com>
2025-02-22 00:20:56 +0530

    Add database schema and tables for mock interviews, questions, user answers, and newsletters; include dotenv for environment variables and implement text-to-speech functionality in QuestionSection component

--- drizzle.config.js
+++ drizzle.config.js
@@ -6,7 +6,20 @@ export default defineConfig({
     dialect: "postgresql",
     schema: "./utils/schema.ts", // Change .js -> .ts
     dbCredentials: {
-      url: 'postgresql://neondb_owner:npg_JP8XIZFxq6kg@ep-fancy-sun-a1uav4xt-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
     },
   });
   
+// const { defineConfig } = require("drizzle-kit");
+// require('dotenv').config();
+
+// module.exports = defineConfig({
+//   schema: "./utils/schema.js",
+//   out: "./migrations",
+//   dialect: "pg",
+//   verbose: true,
+//   strict: true,
+//   dbCredentials: {
+//     connectionString: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
+//   }
+// });
--- utils/schema.ts
+++ utils/schema.ts
@@ -1,4 +1,5 @@
-import { serial, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { serial, text, varchar,timestamp } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
 
 export const MockInterview = pgTable('mockInterview', {
     id: serial('id').primaryKey(),
@@ -10,3 +11,36 @@ export const MockInterview = pgTable('mockInterview', {
     createdAt: varchar('createdAt', { length: 255 }),
     mockId: varchar('mockId', { length: 255 }).notNull()
 });
+export const Question = pgTable("question", {
+    id: serial("id").primaryKey(),
+    MockQuestionJsonResp: text("MockQuestionJsonResp").notNull(),
+    jobPosition: varchar("jobPosition", { length: 255 }).notNull(),
+    jobDesc: varchar("jobDesc", { length: 1000 }).notNull(),
+    jobExperience: varchar("jobExperience", { length: 255 }).notNull(),
+    typeQuestion: varchar("typeQuestion", { length: 255 }).notNull(),
+    company: varchar("company", { length: 255 }).notNull(),
+    createdBy: varchar("createdBy", { length: 255 }).notNull(),
+    createdAt: timestamp("createdAt").defaultNow(),
+    mockId: varchar("mockId", { length: 255 }).notNull(),
+  });
+  
+  export const UserAnswer = pgTable("userAnswer", {
+    id: serial("id").primaryKey(),
+    mockIdRef: varchar("mockId", { length: 255 }).notNull(),
+    question: text("question").notNull(),
+    correctAns: text("correctAns"),
+    userAns: text("userAns"),
+    feedback: text("feedback"),
+    rating: varchar("rating", { length: 10 }),
+    userEmail: varchar("userEmail", { length: 255 }),
+    createdAt: timestamp("createdAt").defaultNow(),
+  });
+  
+  export const Newsletter = pgTable("newsletter", {
+    id: serial("id").primaryKey(),
+    newName: varchar("newName", { length: 255 }),
+    newEmail: varchar("newEmail", { length: 255 }),
+    newMessage: text("newMessage"),
+    createdAt: timestamp("createdAt").defaultNow(),
+  });
+  
